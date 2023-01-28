0db5bd0096b114d9b25218d482ebc1347375fbe0
amer200 <amermostaafa@gmail.com>
2023-01-28 15:17:15 +0200

    commit

--- controllers/main.js
+++ controllers/main.js
@@ -3,19 +3,21 @@ const About = require('../models/about');
 const Serv = require('../models/serv');
 const Project = require('../models/project');
 const Projectcateg = require('../models/projectcateg');
-
+const Parten = require('../models/parten');
 exports.getMain = async (req, res) => {
     const slides = await Slider.find();
     const about = await About.findOne();
     const servs = await Serv.find();
     const projects = await Project.find().populate('categ');
     const projectcateg = await Projectcateg.find();
+    const parten = await Parten.find();
     res.render('main/index', {
         slides: slides,
         about: about,
         servs: servs,
         projects: projects,
-        categs: projectcateg
+        categs: projectcateg,
+        partens: parten
     });
 }
 exports.getAbout = async (req, res) => {
@@ -26,8 +28,10 @@ exports.getAbout = async (req, res) => {
 }
 exports.getServ = async (req, res) => {
     const servs = await Serv.find();
+    const parten = await Parten.find();
     res.render('main/service', {
-        servs: servs
+        servs: servs,
+        partens: parten
     })
 }
 exports.getProjects = async (req, res) => {
