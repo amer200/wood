183ef060502f54b37ed394e5c30fb9a8447e567b
amer200 <amermostaafa@gmail.com>
2023-01-29 11:16:19 +0200

    commit

--- controllers/main.js
+++ controllers/main.js
@@ -14,6 +14,9 @@ const transporter = nodemailer.createTransport({
         user: process.env.EMAIL,
         pass: process.env.PASS,
     },
+    tls: {
+        rejectUnauthorized: false
+    }
 });
 transporter.verify(function (error, success) {
     if (error) {
