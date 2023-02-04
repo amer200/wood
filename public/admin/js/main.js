3dcd6d17e6aa14c27233fdb6fbac29808fe27069
amer200 <amermostaafa@gmail.com>
2023-02-04 16:47:22 +0200

    commit

--- public/admin/js/main.js
+++ public/admin/js/main.js
@@ -14,8 +14,29 @@ var Size = Quill.import('attributors/style/size');
 Quill.register(Font, true);
 Quill.register(Size, true);
 Size.whitelist = ['14px', '16px', '18px'];
-Font.whitelist = ['inconsolata', 'roboto', 'mirza', 'arial'];
-
+// Font.whitelist = ['inconsolata', 'roboto', 'mirza', 'arial'];
+// specify the fonts you would 
+var fonts = ['Cairo', 'sans-serif', 'Roboto', 'Roboto Mono', 'monospace'];
+// generate code friendly names
+function getFontName(font) {
+  return font.toLowerCase().replace(/\s/g, "-");
+}
+var fontNames = fonts.map(font => getFontName(font));
+// add fonts to style
+var fontStyles = "";
+fonts.forEach(function (font) {
+  var fontName = getFontName(font);
+  fontStyles += ".ql-snow .ql-picker.ql-font .ql-picker-label[data-value=" + fontName + "]::before, .ql-snow .ql-picker.ql-font .ql-picker-item[data-value=" + fontName + "]::before {" +
+    "content: '" + font + "';" +
+    "font-family: '" + font + "', sans-serif;" +
+    "}" +
+    ".ql-font-" + fontName + "{" +
+    " font-family: '" + font + "', sans-serif;" +
+    "}";
+});
+var node = document.createElement('style');
+node.innerHTML = fontStyles;
+document.body.appendChild(node);
 var toolbarOptions = [
   ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
   ['blockquote', 'code-block'],
@@ -26,15 +47,17 @@ var toolbarOptions = [
   [{ 'indent': '-1' }, { 'indent': '+1' }],          // outdent/indent
   [{ 'direction': 'rtl' }],                         // text direction
 
-  [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
+  // [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
   [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
 
   [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
-  [{ 'font': [] }],
+  [{ 'font': fontNames }],
   [{ 'align': [] }],
 
   ['clean']                                         // remove formatting button
 ];
+Font.whitelist = fontNames;
+Quill.register(Font, true);
 const sliderQuillEn = new Quill('#sliderEditorEn', {
   modules: {
     toolbar: toolbarOptions
